e9b5e137d80c4ecc2c01421a1ad348f03c56de10
Brock Byrd <brockbyrdd@gmail.com>
2020-09-24 18:35:58 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,10 +6,19 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
   # location: project.css("ul.project-meta li a").first.text
+  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", " ").to_i
   binding.pry
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+
+  projects
 end
